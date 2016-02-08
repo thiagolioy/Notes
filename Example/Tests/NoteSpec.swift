7c13f655bce24af6f8c39b4469ff7d3694a92c4b
Thiago Lioy <thiagolioy@Lioy-MacBookPro.local>
2016-02-08 11:17:32 -0200

    adding more tests to note

--- Example/Tests/NoteSpec.swift
+++ Example/Tests/NoteSpec.swift
@@ -19,6 +19,73 @@ class NoteSpec: QuickSpec {
                 expect(nextNote.name) == NoteName.C
                 expect(nextNote.intonation) == NoteIntonation.Sharp
             }
+            it("next of C# should be D") {
+                let note:Note = Note(name:.C,intonation: .Sharp)
+                let nextNote = note.next()
+                expect(nextNote.name) == NoteName.D
+                expect(nextNote.intonation) == NoteIntonation.Natural
+            }
+            it("next of D should be D#") {
+                let note:Note = Note(name:.D,intonation: .Natural)
+                let nextNote = note.next()
+                expect(nextNote.name) == NoteName.D
+                expect(nextNote.intonation) == NoteIntonation.Sharp
+            }
+            it("next of D# should be E") {
+                let note:Note = Note(name:.D,intonation: .Sharp)
+                let nextNote = note.next()
+                expect(nextNote.name) == NoteName.E
+                expect(nextNote.intonation) == NoteIntonation.Natural
+            }
+            it("next of E should be F") {
+                let note:Note = Note(name:.E,intonation: .Natural)
+                let nextNote = note.next()
+                expect(nextNote.name) == NoteName.F
+                expect(nextNote.intonation) == NoteIntonation.Natural
+            }
+            it("next of F should be F#") {
+                let note:Note = Note(name:.F,intonation: .Natural)
+                let nextNote = note.next()
+                expect(nextNote.name) == NoteName.F
+                expect(nextNote.intonation) == NoteIntonation.Sharp
+            }
+            it("next of F# should be G") {
+                let note:Note = Note(name:.F,intonation: .Sharp)
+                let nextNote = note.next()
+                expect(nextNote.name) == NoteName.G
+                expect(nextNote.intonation) == NoteIntonation.Natural
+            }
+            it("next of G should be G#") {
+                let note:Note = Note(name:.G,intonation: .Natural)
+                let nextNote = note.next()
+                expect(nextNote.name) == NoteName.G
+                expect(nextNote.intonation) == NoteIntonation.Sharp
+            }
+            it("next of G# should be A") {
+                let note:Note = Note(name:.G,intonation: .Sharp)
+                let nextNote = note.next()
+                expect(nextNote.name) == NoteName.A
+                expect(nextNote.intonation) == NoteIntonation.Natural
+            }
+            it("next of A should be A#") {
+                let note:Note = Note(name:.A,intonation: .Natural)
+                let nextNote = note.next()
+                expect(nextNote.name) == NoteName.A
+                expect(nextNote.intonation) == NoteIntonation.Sharp
+            }
+            it("next of A# should be B") {
+                let note:Note = Note(name:.A,intonation: .Sharp)
+                let nextNote = note.next()
+                expect(nextNote.name) == NoteName.B
+                expect(nextNote.intonation) == NoteIntonation.Natural
+            }
+            it("next of B should be C") {
+                let note:Note = Note(name:.B,intonation: .Natural)
+                let nextNote = note.next()
+                expect(nextNote.name) == NoteName.C
+                expect(nextNote.intonation) == NoteIntonation.Natural
+            }
+            
         }
     }
 }
